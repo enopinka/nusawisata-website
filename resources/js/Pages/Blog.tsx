2b5ab69482fb59dc6cce24655cf478bb9cabf4cf
Retno Pinka <retnopinka20@gmail.com>
2025-05-04 22:10:25 +0700

    fix: blog list

--- resources/js/Pages/Blog.tsx
+++ resources/js/Pages/Blog.tsx
@@ -13,7 +13,7 @@ type Blog = {
     title: string;
     slug: string;
     content: string;
-    edited_at: Date;
+    created_at: Date;
 };
 
 type BlogsProps = {
@@ -24,27 +24,51 @@ export default function Blog({ blogs }: BlogsProps) {
     return (
         <>
             <AppLayout>
-                <div className="mt-20 max-w-2xl mx-auto">
-                    <p className="my-8 text-5xl">Blogs</p>
-                    <div className="grid grid-cols-3 gap-4">
-                        {blogs.map((blog) => (
-                            <Link key={blog.id} href={`/blog/${blog.slug}`}>
-                                <Card className="hover:shadow-md hover:scale-105 transition-transform duration-300">
-                                    <img
-                                        src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
-                                        className="w-full rounded-t-lg"
-                                    />
-                                    <CardHeader>
-                                        <CardTitle className="hover:underline">
-                                            {blog.title}
-                                        </CardTitle>
-                                        <CardDescription>
-                                            {blog.content.substring(0, 100)}...
-                                        </CardDescription>
-                                    </CardHeader>
-                                </Card>
-                            </Link>
-                        ))}
+                <div className="bg-custom-cream py-20 text-custom-blue">
+                    <div className="mt-20 max-w-5xl mx-auto ">
+                        <div className="my-4 space-y-2">
+                            <p className="text-4xl font-montserrat font-bold">
+                                Blogs
+                            </p>
+                            <p className="font-jost  text-custom-blue text-lg">
+                                Temukan berbagai artikel informatif, tips
+                                perjalanan, dan cerita seru seputar destinasi
+                                wisata terbaik dari kami.
+                            </p>
+                        </div>
+                        <div>
+                            {blogs.map((blog) => (
+                                <div key={blog.id}>
+                                    <Link href={`/blog/${blog.slug}`}>
+                                        <div className="w-full flex gap-4 h-32 justify-start items-center">
+                                            <div>
+                                                <img
+                                                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
+                                                    className="w-32 h-auto rounded-lg"
+                                                />
+                                            </div>
+                                            <div className="space-y-2">
+                                                <p className="text-2xl font-semibold">
+                                                    {blog.title}
+                                                </p>
+                                                <p>
+                                                    {new Date(
+                                                        blog.created_at
+                                                    ).toLocaleString("id-ID", {
+                                                        day: "numeric",
+                                                        month: "long",
+                                                        year: "numeric",
+                                                        hour: "2-digit",
+                                                        minute: "2-digit",
+                                                    })}
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <hr />
+                                    </Link>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </AppLayout>
