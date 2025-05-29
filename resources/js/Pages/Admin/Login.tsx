41cf29a6409eae25fe79949859d195320742cb39
Rizal Anggoro <gnoogler4@gmail.com>
2025-05-29 11:07:11 +0700

    chore: redesign login page

--- resources/js/Pages/Admin/Login.tsx
+++ resources/js/Pages/Admin/Login.tsx
@@ -1,6 +1,12 @@
-import { Alert, AlertDescription, AlertTitle } from "@/Components/ui/alert";
 import { Button } from "@/Components/ui/button";
-import { Card, CardHeader, CardTitle } from "@/Components/ui/card";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/Components/ui/card";
 import {
     Form,
     FormControl,
@@ -10,11 +16,13 @@ import {
     FormMessage,
 } from "@/Components/ui/form";
 import { Input } from "@/Components/ui/input";
+import { Toaster } from "@/Components/ui/sonner";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { router, usePage } from "@inertiajs/react";
-import { AlertCircle, Eye, EyeOff, X } from "lucide-react";
-import { useState } from "react";
+import { Head, router, usePage } from "@inertiajs/react";
+import { EyeIcon, EyeOffIcon, Loader2Icon } from "lucide-react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 const formSchema = z.object({
@@ -29,119 +37,125 @@ const formSchema = z.object({
 
 export default function Login() {
     const { errors } = usePage().props;
-    const [password, setPassword] = useState(false);
-    const [showAlert, setShowAlert] = useState(!!errors.password);
+    const [showPassword, setShowPassword] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        if (errors.password)
+            toast.error("Terjadi Kesalahan!", {
+                description: errors.password,
+                position: "top-right",
+            });
+    }, [errors]);
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
     });
 
     const onSubmit = (values: z.infer<typeof formSchema>) => {
+        setIsLoading(true);
         router.post(route("admin.login"), values, {
-            onSuccess: () => {
-                console.log("berhasil login");
-            },
-            onError: (e) => {
-                console.log(e);
-            },
+            onFinish: () => setIsLoading(false),
         });
     };
+
     return (
         <>
-            {showAlert && errors.password && (
-                <div className="fixed top-4 left-1/2 -translate-x-1/2 max-w-2xl w-full">
-                    <Alert
-                        variant="destructive"
-                        className="flex justify-between items-center"
-                    >
-                        <div className="flex gap-4 items-center">
-                            <AlertCircle className="h-4 w-4" />
-                            <div>
-                                <AlertTitle>Error</AlertTitle>
-                                <AlertDescription>
-                                    {errors.password}
-                                </AlertDescription>
-                            </div>
-                        </div>
-                        <div>
-                            <button
-                                type="button"
-                                onClick={() => setShowAlert(false)}
-                            >
-                                <X />
-                            </button>
-                        </div>
-                    </Alert>
-                </div>
-            )}
-            <div className="flex justify-center font-montserrat h-screen items-center">
-                <Card className="max-w-2xl w-full p-6 shadow-md">
-                    <CardHeader>
-                        <CardTitle className="text-center">Login</CardTitle>
-                    </CardHeader>
-                    <Form {...form}>
-                        <form
-                            onSubmit={form.handleSubmit(onSubmit)}
-                            className="space-y-8"
-                        >
-                            <FormField
-                                control={form.control}
-                                name="email"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Alamat Email</FormLabel>
-                                        <FormControl>
-                                            <Input {...field} />
-                                        </FormControl>
+            <Head title="Autentikasi Admin" />
 
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="password"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Kata Sandi</FormLabel>
-                                        <FormControl>
-                                            <div className="relative">
+            <div className="bg-custom-blue h-screen w-full flex items-center">
+                <Card className="mx-auto max-w-lg shadow-none">
+                    <CardHeader>
+                        <CardTitle>Masuk</CardTitle>
+                        <CardDescription>
+                            Masukkan beberapa informasi berikut untuk mengakses
+                            halaman dashboard admin.
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <Form {...form}>
+                            <form
+                                onSubmit={(e) => e.preventDefault()}
+                                className="space-y-4"
+                            >
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Alamat Email</FormLabel>
+                                            <FormControl>
                                                 <Input
                                                     {...field}
-                                                    type={
-                                                        password
-                                                            ? "text"
-                                                            : "password"
-                                                    }
-                                                    className="pr-10"
+                                                    placeholder="Masukkan alamat email"
                                                 />
-                                                <button
-                                                    type="button"
-                                                    onClick={() =>
-                                                        setPassword(!password)
-                                                    }
-                                                    className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
-                                                >
-                                                    {password ? (
-                                                        <EyeOff size={20} />
-                                                    ) : (
-                                                        <Eye size={20} />
-                                                    )}
-                                                </button>
-                                            </div>
-                                        </FormControl>
+                                            </FormControl>
 
-                                        <FormMessage />
-                                    </FormItem>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Kata Sandi</FormLabel>
+                                            <FormControl>
+                                                <div className="flex items-center gap-2">
+                                                    <Input
+                                                        {...field}
+                                                        type={
+                                                            showPassword
+                                                                ? "text"
+                                                                : "password"
+                                                        }
+                                                        className="flex-1"
+                                                        placeholder="Masukkan kata sandi"
+                                                    />
+
+                                                    <Button
+                                                        size={"icon"}
+                                                        variant={"secondary"}
+                                                        onClick={() =>
+                                                            setShowPassword(
+                                                                (prev) => !prev,
+                                                            )
+                                                        }
+                                                    >
+                                                        {showPassword ? (
+                                                            <EyeOffIcon />
+                                                        ) : (
+                                                            <EyeIcon />
+                                                        )}
+                                                    </Button>
+                                                </div>
+                                            </FormControl>
+
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </form>
+                        </Form>
+                    </CardContent>
+                    <CardFooter>
+                        <div className="flex w-full justify-end">
+                            <Button
+                                onClick={() => form.handleSubmit(onSubmit)()}
+                                disabled={isLoading}
+                            >
+                                {isLoading && (
+                                    <Loader2Icon className="animate-spin" />
                                 )}
-                            />
-                            <div className="w-full flex justify-end">
-                                <Button type="submit">Submit</Button>
-                            </div>
-                        </form>
-                    </Form>
+                                Masuk
+                            </Button>
+                        </div>
+                    </CardFooter>
                 </Card>
             </div>
+
+            <Toaster richColors theme="light" />
         </>
     );
 }
